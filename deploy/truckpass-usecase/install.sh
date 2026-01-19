60f24e30187c4a87b7337075587d12b993cf7db8
Mohan E <mohanraj1715@gmail.com>
2026-01-19 11:46:29 +0530

    [MOSIP-43000] Updated deployment scripts.
    Signed-off-by: Mohanraj209 <mohan1715@gmail.com>

--- deploy/truckpass-usecase/install.sh
+++ deploy/truckpass-usecase/install.sh
@@ -34,8 +34,8 @@ function installing_truckpass-usecase() {
 
   API_HOST=$(kubectl get cm global -o jsonpath={.data.mosip-api-internal-host})
 
-  echo Installing Truckpass-usecase. Will wait till service gets installed.
-  helm -n $NS install truckpass-usecase /home/techno-408/IdeaProjects/inji-mock-services/helm/truckpass-usecase --set image.repository=mohanraj209/inji-usecase --set image.tag=truckpass --set istio.corsPolicy.allowOrigins\[0\].prefix=https://$truckpass_usecase_host --wait --version $CHART_VERSION -f override-values.yaml
+#  echo Installing Truckpass-usecase. Will wait till service gets installed.
+#  helm -n $NS install truckpass-usecase /home/techno-408/IdeaProjects/inji-mock-services/helm/truckpass-usecase --set image.repository=mohanraj209/inji-usecase --set image.tag=truckpass --set istio.corsPolicy.allowOrigins\[0\].prefix=https://$truckpass_usecase_host --wait --version $CHART_VERSION -f override-values.yaml
 
   echo Installing Truckpass-UI. Will wait till the UI gets installed.
   helm -n $NS install truckpass-ui /home/techno-408/IdeaProjects/inji-mock-services/helm/truckpass-ui --set image.repository=mohanraj209/truckpass-ui --set image.tag=truckpass --set truckpass.apiUrl=https://$API_HOST/v1/ --set istio.hosts\[0\]=$truckpass_usecase_host --version $CHART_VERSION
