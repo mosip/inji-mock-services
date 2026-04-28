9d764a250efa6a66b45fd67d5922dd17e7af36bb
sarupmoni <93664377+sarupmoni@users.noreply.github.com>
2026-04-28 19:23:54 +0530

    [INJICERT-741] - fix Incorrect text
    Signed-off-by: sarupmoni <93664377+sarupmoni@users.noreply.github.com>

--- truck-pass/src/commans/LandingPage.tsx
+++ truck-pass/src/commans/LandingPage.tsx
@@ -50,7 +50,7 @@ const LandingPage = () => {
               <div className="flex flex-col bg-[#FFFFFF] w-[380px] h-auto border-0 rounded-xl items-center py-7 shadow-2xl space-y-5 text-center justify-between">
                 <img src={driver_user_icon} alt="Driver Registration Icon" className="h-10 border-0 p-2 rounded-md shadow-lg" />
                 <p className="font-[600]">{t('landingPage.driverRegistration')}</p>
-                <p className="text-[12px]">{t('landingPage.driverRegSubTitle')}</p>
+                <p className="text-[12px] w-[90%] text-center">{t('landingPage.driverRegSubTitle')}</p>
                 <button onClick={moveToDriverRegistration} className="bg-[#006DE7] w-[90%] text-[10px] font-[600] py-[2%] text-center rounded-[5px] text-[#FFFFFF] cursor-pointer">
                   {t('landingPage.registerAsDriver')}
                 </button>
@@ -59,7 +59,7 @@ const LandingPage = () => {
               <div className="flex flex-col bg-[#FFFFFF] w-[380px] h-auto border-0 rounded-xl items-center py-7 shadow-2xl space-y-5 justify-between">
                 <img src={request_truck_pass_icon} className="h-10 border-0 p-2 rounded-md shadow-lg" />
                 <p className="font-[600]">{t('landingPage.requestTruckPass')}</p>
-                <p className="text-[12px]">{t('landingPage.requestTruckPassSubTitle')}</p>
+                <p className="text-[12px] w-[90%] text-center">{t('landingPage.requestTruckPassSubTitle')}</p>
                 <button onClick={moveToRequestTruckPass} className="bg-[#006DE7] w-[90%] text-[10px] font-[600] py-[2%] text-center rounded-[5px] text-[#FFFFFF] cursor-pointer">
                   {t('landingPage.login')}
                 </button>
